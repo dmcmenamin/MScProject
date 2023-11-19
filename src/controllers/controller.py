4b4c020cbd49c92dd13eaea20b15a3231bdb6348
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-11-19 14:11:59 +0000

    Cleaned Up Controller & Presentation Code

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -9,58 +9,24 @@ from src.powerpoint.presentation import PowerPointPresentation
 #           "a thank you slide. Please also include a slide with a list of references.")
 
 prompt = ("f {presenter_name} is preparing to give a presentation on {topic} to {audience_size} people. "
-          "The presentation will last {time} minutes. At the end of the presentation, wants the audience to be "
-          "able to {audience_outcome}. Please create a slide deck for {presenter_name} to use, which should "
-          "include title and content. Please also include notes for {presenter_name} for each slide. "
-          "Please also ensure that each title slide, content and notes are clearly labelled. Finally please also "
+          "The presentation will last {time} minutes. At the end of the presentation, the audience will be expected to "
+          "{audience_outcome}. Create a slide deck for {presenter_name} to use, which should "
+          "include title and content. Also include notes for {presenter_name} for each slide. "
+          "Ensure that each title slide, content and notes are clearly labelled. In the notes section, define how long"
+          "the presenter should spend on that slide, ensuring the total time adds up to {time}. Finally please also "
           "include a slide with a list of references.")
 
-
-def slice_presentation(presentation):
-    slide_pages = []
-    current_slide = []
-
-    for presentation_line in presentation.splitlines():
-        if presentation_line.startswith("Slide"):
-            slide_pages.append(current_slide)
-            current_slide = []
-        else:
-            current_slide.append(presentation_line)
-
-    return slide_pages
-
-
-def get_slide_sections(slide_sections):
-    slide_title = ""
-    slide_subtitle = ""
-    slide_content = ""
-    slide_notes = ""
-    last_section = ""
-    for section in slide_sections:
-        if len(section) >= 1:
-            if section.startswith("Title:"):
-                slide_title = section.replace("Title:", "").strip()
-            elif section.startswith("Subtitle:"):
-                slide_subtitle = section.replace("Subtitle:", "").strip()
-            elif section.startswith("Content:"):
-                slide_content = section.replace("Content:", "").replace("-", "").strip() + "\n"
-                last_section = "Content"
-            elif section.startswith("Notes"):
-                slide_content = slide_content.strip() + " \n"
-                last_section = "Notes"
-            elif last_section == "Content":
-                slide_content += section.replace("-", "").strip() + "\n"
-            elif last_section == "Notes":
-                slide_notes += section.strip() + "\n"
-    return slide_title, slide_subtitle, slide_content, slide_notes
-
-
 if __name__ == "__main__":
-    presenter_name = input("What is the name of the presenter? ")
-    topic = input("What do you want a presentation on? ")
-    audience_size = input("How many people will be in the audience? ")
-    time = input("How many minutes will the presentation be? ")
-    audience_outcome = input("What do you want the audience to be able to do at the end of the presentation? ")
+    presenter_name = "Darren McMenamin"
+    topic = "The importance of Python as a programming language"
+    audience_size = "100"
+    time = "15"
+    audience_outcome = "Understand the importance of Python as a programming language"
+    # presenter_name = input("What is the name of the presenter? ")
+    # topic = input("What do you want a presentation on? ")
+    # audience_size = input("How many people will be in the audience? ")
+    # time = input("How many minutes will the presentation be? ")
+    # audience_outcome = input("What do you want the audience to be able to do at the end of the presentation? ")
 
     prompt = prompt.format(presenter_name=presenter_name,
                            topic=topic, audience_size=audience_size, time=time, audience_outcome=audience_outcome)
@@ -68,31 +34,11 @@ if __name__ == "__main__":
     orchestrator = Orchestrator("chat", "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",
                                 "gpt-3.5-turbo-1106")
 
-    slide_presentation = slice_presentation(orchestrator.call_large_language_model().get_presentation_slides(prompt))
+    powerpoint_presentation = PowerPointPresentation(
+        orchestrator.call_large_language_model().get_presentation_slides(prompt))
 
-    # output slide_presentation to a file
-    with open(topic + '.txt', 'w') as f:
-        for item in slide_presentation:
-            f.write("%s\n" % item)
-
-    print(slide_presentation)
-    powerpoint_presentation = PowerPointPresentation()
-
-    for sections in slide_presentation:
-
-        if len(sections) == 0:
-            continue
-        title, subtitle, content, notes = get_slide_sections(sections)
-
-        if title and subtitle:
-            powerpoint_presentation.add_slide(0, title=title, text=subtitle, notes=notes)
-        elif title and content:
-            powerpoint_presentation.add_slide(1, title=title, text=content, notes=notes)
-        elif title:
-            powerpoint_presentation.add_slide(0, title=title, notes=notes)
-        elif content:
-            powerpoint_presentation.add_slide(1, text=content, notes=notes)
-        else:
-            powerpoint_presentation.add_slide(0, title="Slide", notes=notes)
+    # Save the returns as a text file for debugging purposes
+    with open(topic + ".txt", "w") as f:
+        f.write(powerpoint_presentation.presentation)
 
     powerpoint_presentation.save(topic + ".pptx")
--- src/powerpoint/presentation.py
+++ src/powerpoint/presentation.py
@@ -20,12 +20,104 @@ SLIDE_PICTURE_WITH_CAPTION_LAYOUT = 8
 class PowerPointPresentation:
 
     # Initialise the class
-    def __init__(self):
-        self.presentation = Presentation()
+    def __init__(self, presentation=None):
+        if presentation:
+            self.presentation = Presentation(presentation)
+            # self.slides = self.__slice_presentation(presentation)
+            self.populate_presentation()
+        else:
+            self.presentation = Presentation()
 
     # Return a string representation of the class
     def __str__(self):
         return f"PowerPointPresentation(presentation={self.presentation})"
+
+    # Private Method to slice a presentation into slides
+    # Inputs: presentation
+    # presentation is a string - Raises ValueError if presentation is empty
+    # Returns a list of slides
+    def __slice_presentation(self, presentation):
+        if not presentation:
+            raise ValueError("Presentation cannot be empty.")
+        else:
+            slide_pages = []
+            current_slide = []
+
+            for presentation_line in presentation.splitlines():
+                if presentation_line.startswith("Slide"):
+                    slide_pages.append(current_slide)
+                    current_slide = []
+                else:
+                    current_slide.append(presentation_line)
+
+            return slide_pages
+
+    # Private Method to get slide content
+    # Inputs: slide_pages
+    # slide_pages is a list of slides - Raises ValueError if slide_pages is empty
+    # Returns a title, subtitle, content and notes for each slide
+    def __get_slide_content(self, slide_pages):
+        if not slide_pages:
+            raise ValueError("Slide pages cannot be empty.")
+        else:
+            slide_title = ""
+            slide_subtitle = ""
+            slide_content = ""
+            slide_notes = ""
+            last_section = ""
+            for section in slide_pages:
+                if len(section) >= 1:
+                    if section.startswith("Title:"):
+                        slide_title = section.replace("Title:", "").strip()
+                    elif section.startswith("Subtitle:"):
+                        slide_subtitle = section.replace("Subtitle:", "").strip()
+                    elif section.startswith("Content:"):
+                        slide_content = section.replace("Content:", "").replace("-", "").strip() + "\n"
+                        if "(No content required)".lower() in slide_content.lower():
+                            slide_content = ""
+                        last_section = "Content"
+                    elif section.startswith("Notes"):
+                        slide_notes = section.strip() + " \n"
+                        last_section = "Notes"
+                    elif last_section == "Content":
+                        slide_content += section.replace("-", "").strip() + "\n"
+                    elif last_section == "Notes":
+                        slide_notes += section.strip() + "\n"
+            return slide_title, slide_subtitle, slide_content, slide_notes
+
+    # Private Method to set the slide layouts
+    # Inputs: None
+    # Returns the best slide layout for the presentation
+    def __set_layouts(self, title, subtitle, content, image, notes):
+        if title and subtitle:
+            return SLIDE_TITLE_LAYOUT
+        elif title and content:
+            return SLIDE_TITLE_AND_CONTENT_LAYOUT
+        elif title:
+            return SLIDE_TITLE_ONLY_LAYOUT
+        elif content:
+            return SLIDE_CONTENT_WITH_CAPTION_LAYOUT
+        elif image:
+            return SLIDE_PICTURE_WITH_CAPTION_LAYOUT
+        else:
+            return SLIDE_BLANK_LAYOUT
+
+    # Populate the presentation with content
+    # Inputs: title, subtitle, content, image, notes
+    # title is a string - defaults to None
+    # subtitle is a string - defaults to None
+    # content is a string - defaults to None
+    # image is a string - defaults to None
+    # notes is a string - defaults to None
+    # Returns the populated presentation
+    def populate_presentation(self):
+        sections = self.__slice_presentation(self.presentation)
+        for sections in self.presentation:
+
+            if len(sections) == 0:
+                continue
+            title, subtitle, content, notes = self.__get_slide_content(sections)
+            self.add_slide(title=title, subtitle=subtitle, text=content, notes=notes)
 
     # Add a slide to the presentation
     # Inputs: slide_layout, title, text, image, notes
@@ -36,11 +128,13 @@ class PowerPointPresentation:
     # image is a string - defaults to None - Raises ValueError if image is not provided for picture with caption layout
     # notes is a string - defaults to None
     # Returns the created slide
-    def add_slide(self, slide_layout, title=None, text=None, image=None, notes=None):
-        if slide_layout not in range(0, 9):
-            raise ValueError("Slide layout must be between 0 and 8.")
-        elif not isinstance(slide_layout, int):
-            raise ValueError("Slide layout must be an integer.")
+    def add_slide(self, title=None, subtitle=None, text=None, image=None, notes=None):
+        # if slide_layout not in range(0, 9):
+        #     raise ValueError("Slide layout must be between 0 and 8.")
+        # elif not isinstance(slide_layout, int):
+        #     raise ValueError("Slide layout must be an integer.")
+
+        slide_layout = self.__set_layouts(title, subtitle, text, image, notes)
 
         created_slide = self.presentation.slides.add_slide(self.presentation.slide_layouts[slide_layout])
         if title:
