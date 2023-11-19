56d4a2b8aaeec7bc6e80d8fe927ce0d6a4459c53
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-11-19 21:17:50 +0000

    Added Images, but require cleanup on Title And review of slides with no images

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,5 +1,8 @@
 from src.orchestration.orchestrator import Orchestrator
 from src.powerpoint.presentation import PowerPointPresentation
+import requests
+from io import BytesIO
+import os
 
 # prompt = ("f Frank is preparing to give a presentation on {topic} to {audience_size} people. The presentation will "
 #           "last {time} minutes. At the end of the presentation, wants the audience to be able to {audience_outcome}. "
@@ -12,9 +15,35 @@ prompt = ("f {presenter_name} is preparing to give a presentation on {topic} to 
           "The presentation will last {time} minutes. At the end of the presentation, the audience will be expected to "
           "{audience_outcome}. Create a slide deck for {presenter_name} to use, which should "
           "include title and content. Also include notes for {presenter_name} for each slide. "
-          "Ensure that each title slide, content and notes are clearly labelled. In the notes section, define how long"
-          "the presenter should spend on that slide, ensuring the total time adds up to {time}. Finally please also "
-          "include a slide with a list of references.")
+          "Ensure that each title slide, content and notes are clearly labelled. Also provide some image suggestions on"
+          "on a new line on the relevant slides throughout the presentation, identified by keyword IMAGE_SUGGESTION at "
+          "the start of the line. In the notes section, define how long the presenter should spend on that slide, "
+          "ensuring the total time adds up to {time}. Finally please also include a slide with a list of references.")
+
+
+# method that takes in the string, and searches for the keyword IMAGE_SUGGESTION
+# if it finds this, it will take the following line and use it to search for an image
+# it will then return the image url
+def get_ai_image_suggestion(string):
+    for line in string.splitlines():
+        if line.startswith("IMAGE_SUGGESTION"):
+            image_requested = line.split("IMAGE_SUGGESTION: ")[1]
+            image_url = orchestrator.call_large_language_model().get_presentation_image(image_requested.rstrip('.'))
+            response = requests.get(image_url)
+            image = BytesIO(response.content)
+
+            if image_requested.endswith("."):
+                local_path = image_requested + "jpg"
+            else:
+                local_path = image_requested + ".jpg"
+
+            with open(local_path, "wb") as image_file:
+                image_file.write(image.read())
+
+            # line = "Image: " + image_url
+            string = string.replace(line, "Image: " + local_path, 1)
+    return string
+
 
 if __name__ == "__main__":
     presenter_name = "Darren McMenamin"
@@ -34,11 +63,25 @@ if __name__ == "__main__":
     orchestrator = Orchestrator("chat", "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",
                                 "gpt-3.5-turbo-1106")
 
-    powerpoint_presentation = PowerPointPresentation(
-        orchestrator.call_large_language_model().get_presentation_slides(prompt))
+    presentation_string = orchestrator.call_large_language_model().get_presentation_slides(prompt)
+
+    with open("presentation.txt", "w") as f:
+        f.write(presentation_string)
+
+    presentation_string = get_ai_image_suggestion(presentation_string)
+
+    with open(topic + " .txt", "w") as f:
+        f.write(presentation_string)
+
+    powerpoint_presentation = PowerPointPresentation(presentation_string)
 
     # Save the returns as a text file for debugging purposes
-    with open(topic + ".txt", "w") as f:
-        f.write(powerpoint_presentation.presentation)
+    # with open(topic + ".txt", "w") as f:
+    #     f.write(powerpoint_presentation.presentation)
 
     powerpoint_presentation.save(topic + ".pptx")
+
+    # clean up the jpg files
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
