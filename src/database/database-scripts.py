303993c32479136774d721c88a884e17a84ebf34
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-12-09 16:34:54 +0000

    Working on Front End

---
+++ src/database/database-scripts.py
@@ -0,0 +1,18 @@
+import hashlib
+import os
+
+
+# Create a salt for the password
+def create_salt():
+    return os.urandom(32)
+
+
+# Hash the password by concatenating the password and salt and hashing it
+def hash_password(password, salt):
+    return hashlib.sha256(password.encode("utf-8") + salt).hexdigest()
+
+
+# Verify the password by hashing it and comparing it to the hashed password in the database
+def check_password(password, salt, hashed_password):
+    return hash_password(password, salt) == hashed_password
+
