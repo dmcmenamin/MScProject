ab0a1e0ebb4bca63cfdb10213791f502633ed936
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-12-09 16:34:43 +0000

    Working on Front End

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from src.database.connection import MySQLConnection
 from werkzeug.serving import run_simple
 
 app = Flask(__name__)
@@ -7,6 +8,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get user input
+        username = request.form['username']
+        password = request.form['password']
+        # check if user exists
+        user = MySQLConnection.query_return_first_match(username=username)
+        # if user exists and password is correct
+        if user and user.password == password:
+            # login user
+            login_user(user)
+            # redirect to homepage
+            return redirect(url_for('index'))
+        # if user doesn't exist or password is incorrect
+        else:
+            # redirect to login page
+            return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
