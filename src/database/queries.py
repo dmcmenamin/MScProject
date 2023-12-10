45b54377202173757f10936c67f617f0f0c03895
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-12-10 10:35:36 +0000

    Working on Front End

--- src/database/queries.py
+++ src/database/queries.py
@@ -2,11 +2,7 @@ from database_scripts import create_salt, hash_password, check_password
 
 
 # Query to check if the user exists in the database
-def check_user(username):
+def check_user_exists(username):
     return f"SELECT * FROM user WHERE Username = '{username}'"
 
 
-# Query to get the user's username and password from the database
-def verify_user(username, password, salt, hashed_password=None):
-    if check_password(password, salt, hashed_password):
-        return f"SELECT * FROM user WHERE Username = '{username}' AND User_Password = '{password}'"
