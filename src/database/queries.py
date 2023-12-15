09b1623bc7c040b0f240d8b1c29418bff2a4a9c0
Darren McMenamin <mcmenamin.darren@gmail.com>
2023-12-15 14:40:36 +0000

    Added HTML pages for Chatbot, Signup Updated HTML page for HTML
    Updated queries.py

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,8 +1,11 @@
-from database_scripts import create_salt, hash_password, check_password
-
-
 # Query to check if the user exists in the database
 def check_user_exists(username):
     return f"SELECT * FROM user WHERE Username = '{username}'"
+
+
+# Query to insert a new user into the database
+def insert_user(username, user_first_name, user_last_name, user_password, user_salt):
+    return (f"INSERT INTO user (Username, User_First_Name, User_Last_Name, User_Password, User_Salt) "
+            f"VALUES ('{username}', '{user_first_name}', '{user_last_name}', '{user_password}', '{user_salt}')")
 
 
