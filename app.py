437a0199aa4563a2b76b8ecb41e0d48ecfe537b5
Darren McMenamin <dmcmenamin06@qub.ac.uk>
2024-01-02 19:21:19 +0000

    refactoring for Rest API's

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for, request, session
-from werkzeug.serving import run_simple
+from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 
+from src.api.login_endpoint import login
+from src.api.signup_endpoint import signup_get, signup_post
 from src.controllers import controller
 from src.database import queries, database_scripts
 from src.database.connection import RelDBConnection
@@ -16,134 +17,37 @@ def index():
     return render_template('index.html', session=session)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        # get user input
-        username = request.form['username']
-        password = request.form['password']
-        database_connection = RelDBConnection()
-
-        # Check if there is a connection to the database, if there is, check if the user exists
-        if not isinstance(database_connection.connect(), Exception):
-
-            # check if user exists
-            params = (username,)
-            returned_user_information = (database_connection.
-                                         query_return_first_match_with_parameter(queries.check_user_exists(), params))
-            # if user exists
-            if returned_user_information:
-                # get user information
-                user_id = returned_user_information[0]
-                username = returned_user_information[1]
-                user_first_name = returned_user_information[2]
-                user_last_name = returned_user_information[3]
-                user_password = returned_user_information[4]
-                user_salt = returned_user_information[5]
-                # check password
-                if database_scripts.check_password(password, user_salt, user_password):
-                    # successful login user
-
-                    # get api key
-                    find_api_key_query, params = queries.get_api_key(username, "ChatGPT")
-                    returned_api_information = (
-                        database_connection.query_return_first_match_with_parameter(find_api_key_query, params))
-                    # store api key in session
-                    session['api_key'] = returned_api_information[0]
-                    # store user information in session
-                    session['user_id'] = user_id
-                    session['username'] = username
-                    session['first_name'] = user_first_name
-                    session['last_name'] = user_last_name
-                    # successful login user redirect to presentation generator page
-                    return redirect(url_for('presentation_generator'))
-                else:
-                    # if password is incorrect
-                    # redirect to login page
-                    return render_template('index.html', login_error="Password is incorrect")
-            else:
-                # if user does not exist
-                # redirect to login page
-                return render_template('index.html', login_error="User does not exist")
-        else:
-            # can't connect to database for login
-            return render_template('index.html', database_error="Database error. "
-                                                                "Please try again later.")
+    response, status_code = login(request.form)
+    if status_code == 200:
+        session['username'] = response.json['username']
+        session['user_id'] = response.json['user_id']
+        session['first_name'] = response.json['first_name']
+        session['last_name'] = response.json['last_name']
+        session['api_key'] = response.json['api_key']
+        return redirect(url_for('presentation_generator'))
+    else:
+        return render_template('index.html', response=response)
 
 
 @app.route('/signup_endpoint', methods=['GET', 'POST'])
 def signup():
     # if it is a get request
     if request.method == 'GET':
-        available_llms_query = queries.get_available_llms()
-        database_connection = RelDBConnection()
-        # if the connection is not an exception, get the available llm model names
-        # otherwise, return an error message to the user and render the signup page
-        if not isinstance(database_connection.connect(), Exception):
-            returned_llm_model_names = (database_connection.
-                                        query_return_matches_specified(available_llms_query, 100))
-            # convert list of tuples to list of strings
-            for i in range(len(returned_llm_model_names)):
-                returned_llm_model_names[i] = returned_llm_model_names[i][0]
-            # render signup page with list of available llm model names
-            return render_template('signup.html', llm_model_names=list(returned_llm_model_names))
+        response, status_code = signup_get()
+        if status_code == 200:
+            return render_template('signup.html', llm_model_names=response.json['llm_model_names'])
         else:
-            return render_template('signup.html', database_error="Database error. "
-                                                                 "Please try again later.")
+            return render_template('signup.html', response=response.json['error'])
 
+    # if it is a post request
     elif request.method == 'POST':
-        # get user input
-        username = request.form['username']
-        password = request.form['password']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-
-        # get api keys for each llm model name and store in dictionary
-        llm = {}
-        if (request.form.get('api_key_{{llm_model_name}})' is not None) or
-                (request.form.get('api_key_{{llm_model_name}})' is not ""))):
-            llm[request.form['llm_model_name']] = request.form['api_key_{{llm_model_name}}']
-        # check if user exists
-        database_connection = RelDBConnection()
-        params = (username,)
-        returned_user_information = (
-            database_connection.query_return_first_match_with_parameter(queries.check_user_exists(), params))
-        # if user exists
-        if returned_user_information:
-            # get available llm model names
-            # TODO: Clean up reused code
-            available_llms_query = queries.get_available_llms()
-            returned_llm_model_names = (database_connection.
-                                        query_return_matches_specified(available_llms_query, 100))
-            # convert list of tuples to list of strings
-            for i in range(len(returned_llm_model_names)):
-                returned_llm_model_names[i] = returned_llm_model_names[i][0]
-            # redirect to login page
-            return render_template('signup.html', username_taken_error="User already exists",
-                                   llm_model_names=list(returned_llm_model_names))
-        # if user does not exist
-        else:
-            # create salt and hashed password
-            salt, hashed_password = database_scripts.create_salted_user_password(password)
-            # create user in database
-            params = (username, first_name, last_name, hashed_password, salt)
-            database_connection.commit_query_with_parameter(queries.create_user(), params)
-
-            # store api keys in database
-            for key, value in llm.items():
-                params = (username, key, value)
-                database_connection.commit_query_with_parameter(queries.create_api_key(), params)
-
-            # store user information in session
-            session['username'] = username
-            session['user_first_name'] = first_name
-            session['user_last_name'] = last_name
-
-            # successful login user redirect to presentation generator page
+        response, status_code = signup_post(request.form)
+        if status_code == 200:
             return redirect(url_for('presentation_generator'))
-    else:
-        return render_template('signup.html', database_error="Database error. "
-                                                             "Please try again later.")
+        else:
+            return render_template('signup.html', response=response)
 
 
 @app.route('/presentation_generator_endpoint', methods=['GET', 'POST'])
---
+++ src/api/login_endpoint.py
@@ -0,0 +1,67 @@
+from flask import jsonify
+
+from src.database import queries, database_scripts
+from src.database.connection import RelDBConnection
+
+
+def login(data):
+    # Get the data from the request
+    username = data.get("username")
+    password = data.get("password")
+
+    # Check if username and password are empty
+    # If so, raise a ValueError
+    # Otherwise, return the username and password
+    if not username:
+        return jsonify({"error": "Username cannot be empty."}), 400
+    elif not password:
+        return jsonify({"error": "Password cannot be empty."}), 400
+
+    database_connection = RelDBConnection()
+
+    # Check if there is a connection to the database, if there is, check if the user exists
+    try:
+        # check if user exists
+        params = (username,)
+        returned_user_information = (database_connection.
+                                     query_return_first_match_with_parameter(queries.check_user_exists(), params))
+        # if user exists
+        if returned_user_information:
+            # get user information
+            user_id, username, user_first_name, user_last_name, user_password, user_salt = returned_user_information
+            # check password
+            if database_scripts.check_password(password, user_salt, user_password):
+                # successful login user
+                # get api key
+                find_api_key_query, params = queries.get_api_key(username, "ChatGPT")
+                returned_api_information = (
+                    database_connection.query_return_first_match_with_parameter(find_api_key_query, params))
+                response_value = {"api_key": returned_api_information[0],
+                                  "user_id": user_id,
+                                  "username": username,
+                                  "first_name": user_first_name,
+                                  "last_name": user_last_name}
+                return jsonify(response_value), 200
+            else:
+                # if password is incorrect
+                # redirect to login page
+                response_value = {"error": "Password is incorrect."}
+                return jsonify(response_value), 401
+        else:
+            # if user does not exist
+            # redirect to login page
+            response_value = {"error": "User does not exist."}
+            return jsonify(response_value), 401
+    except ConnectionError as e:
+        # can't connect to database for login
+        response_value = {"error": f"fDatabase error: {str(e)}. Please try again later."}
+        return jsonify(response_value), 500
+    except AttributeError as e:
+        # Catching attribute error here
+        response_value = {"error": f"Database error: {str(e)}. Please try again later."}
+        return jsonify(response_value), 500
+    except Exception as e:
+        # Catching any other errors
+        response_value = {"error": f" {str(e)}. "}
+        return jsonify(response_value), 500
+
