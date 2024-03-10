61cbca9ad478fea77e75fa33b67041dd18cc1e38
Darren McMenamin <dmcmenamin06@qub.ac.uk>
2024-03-10 19:08:46 +0000

    Continued Clean Up

--- configs/config.py
+++ configs/config.py
@@ -3,8 +3,8 @@ MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_DEFAULT_SENDER = 'noreply@dynapoint.com'
-MAIL_USERNAME = 'dynapoint06@gmail.com'
-MAIL_PASSWORD = 'eatnzqjislmvbdzz'
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
 
 SECURITY_PASSWORD_SALT = 'security_password_salt'
 
